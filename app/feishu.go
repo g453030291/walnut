98bcb21c383a3100bc0fb104ef7cd4a0361d1682
g453030291 <453030291@qq.com>
2023-06-19 18:08:24 -0700

    push test

--- app/feishu.go
+++ app/feishu.go
@@ -1,18 +1,59 @@
 package app
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/tidwall/gjson"
+	"io"
 	"net/http"
 )
 
 func Fmsg(c *gin.Context) {
-	var data map[string]interface{}
-	c.BindJSON(&data)
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "invilid request json"})
+	}
 
-	fmt.Printf("data: %v\n", data)
+	c.JSON(http.StatusOK, sendMsg(string(body)))
+}
 
-	c.JSON(http.StatusOK, gin.H{
-		"challenge": data["challenge"],
-	})
+// 飞书认证challenge方法
+func challenge(data string) gin.H {
+	return gin.H{
+		"challenge": gjson.Get(string(data), "challenge").String(),
+	}
+}
+
+// 接收,回复飞书消息
+func sendMsg(body string) gin.H {
+	url := "https://open.feishu.cn/open-apis/im/v1/messages"
+
+	content := gjson.Get(body, "event.message.content").String()
+	// 收到的消息
+	text := gjson.Get(content, "text").String()
+	fmt.Printf("收到的消息:%s\n", text)
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader([]byte("test send msg")))
+	if err != nil {
+		fmt.Printf("err: %s\n", err)
+		return nil
+	}
+
+	req.Header.Set("Authorization", "Bearer t-"+gjson.Get(body, "event.message.content").String())
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("err: %s\n", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	fmt.Printf("resp: %s\n", resp)
+
+	return gin.H{
+		"result": text,
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module walnut
 go 1.20
 
 require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/bytedance/sonic v1.8.7 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -12,14 +13,20 @@ require (
 	github.com/go-playground/validator/v10 v10.12.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.16.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.3 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.47.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect

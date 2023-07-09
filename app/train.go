1ea6f58be03090d7d56bc733a9a2c816398995c5
g453030291 <453030291@qq.com>
2023-07-09 08:48:36 -0700

    monitor train ticket

--- app/train.go
+++ app/train.go
@@ -3,25 +3,45 @@ package app
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/valyala/fasthttp"
+	"github.com/tidwall/gjson"
 	"net/http"
+	"strings"
+	"walnut/util"
 )
+
+var alert = ""
 
 // Home 回家车票监控
 // 北京->太原
 func Home(c *gin.Context) {
-	req := fasthttp.AcquireRequest()
-	defer fasthttp.ReleaseRequest(req)
+	headers := map[string]string{
+		"Cookie": "_jc_save_fromDate=2023-07-14; _jc_save_toDate=2023-07-04",
+	}
+	result := util.HttpReq("GET", "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2023-07-14&leftTicketDTO.from_station=BJP&leftTicketDTO.to_station=TYV&purpose_codes=ADULT", headers, nil)
 
-	req.SetRequestURI("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2023-07-14&leftTicketDTO.from_station=BJP&leftTicketDTO.to_station=TYV&purpose_codes=ADULT")
-	req.Header.SetMethod("GET")
-
-	resp := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseResponse(resp)
-
-	if err := fasthttp.Do(req, resp); err != nil {
-		fmt.Println("Error in Do:", err)
+	respData := string(result)
+	httpStatus := gjson.Get(respData, "httpstatus")
+	if httpStatus.Int() != 200 {
+		print("12306接口请求失败")
 	}
 
-	c.Data(http.StatusOK, "application/json; charset=utf-8", resp.Body())
+	trainResult := gjson.Get(respData, "data.result")
+
+	trainResult.ForEach(func(key, value gjson.Result) bool {
+		dataStr := strings.Split(value.String(), "|")
+		// 二等 一等 商务
+		if dataStr[3] == "G613" {
+			alertMsg := map[string]interface{}{
+				"msg_type": "text",
+				"content": map[string]string{
+					"text": fmt.Sprintf("车次:%s 商务:%s 一等:%s 二等:%s", dataStr[3], dataStr[32], dataStr[31], dataStr[30]),
+				},
+			}
+			util.HttpReq("POST", alert, nil, alertMsg)
+			return false
+		}
+		return true
+	})
+
+	c.Data(http.StatusOK, "application/json; charset=utf-8", result)
 }
--- util/http_get.go
+++ util/http_get.go
@@ -1,25 +1,47 @@
 package util
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 )
 
-func HttpGet(url string) string {
-	resp, err := http.Get(url)
-
-	if err != nil {
-		log.Println(err)
+// HttpReq 请求封装
+func HttpReq(method string, url string, headers map[string]string, param map[string]interface{}) []byte {
+	client := &http.Client{}
+	var byteParam io.Reader
+	if param != nil {
+		jsonBytes, err := json.Marshal(param)
+		if err != nil {
+			panic(err)
+		}
+		byteParam = bytes.NewBuffer(jsonBytes)
 	}
 
+	req, err := http.NewRequest(method, url, byteParam)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//set header
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	//发送请求
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
 	defer resp.Body.Close()
 
+	//读取响应
 	body, err := io.ReadAll(resp.Body)
-
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
 
-	return string(body)
+	return body
 }

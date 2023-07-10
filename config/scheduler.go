8ac88d01e050013417449e9bfc71483bc04b49b9
g453030291 <453030291@qq.com>
2023-07-09 18:20:43 -0700

    add scheduler

--- config/scheduler.go
+++ config/scheduler.go
@@ -9,7 +9,7 @@ import (
 func CronConfig(scheduler *gocron.Scheduler) {
 	// Every starts the job immediately and then runs at the
 	// specified interval
-	job, err := scheduler.Every(1).Minutes().Do(app.GoHome())
+	job, err := scheduler.Every(10).Minutes().Do(app.GoHome())
 	fmt.Println(job)
 	if err != nil {
 		fmt.Println(err)
